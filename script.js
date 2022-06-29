7cfaf331797a620a2bb8125d279394974ac078a1
BDK27 <briansam195@gmail.com>
2022-06-29 14:59:54 +0800

    completed

---
+++ script.js
@@ -0,0 +1,80 @@
+let whiteText1 = document.getElementById('changeColor1');
+let blackText1 = document.getElementById('changeColor1');
+let whiteText2 = document.getElementById('changeColor2');
+let blackText2 = document.getElementById('changeColor2');
+let whiteText3 = document.getElementById('changeColor3');
+let blackText3 = document.getElementById('changeColor3');
+let whiteText4 = document.getElementById('changeColor4');
+let blackText4 = document.getElementById('changeColor4');
+let whiteText5 = document.getElementById('changeColor5');
+let blackText5 = document.getElementById('changeColor5');
+let whiteText6 = document.getElementById('changeColor6');
+let blackText6 = document.getElementById('changeColor6');
+let whiteText7 = document.getElementById('changeColor7');
+let blackText7 = document.getElementById('changeColor7');
+
+function over1(){
+    whiteText1.style.color='#fff';
+}
+function out1(){
+    blackText1.style.color='#000';
+}
+
+function over2(){
+    whiteText2.style.color='#fff';
+}
+function out2(){
+    blackText2.style.color='#000';
+}
+
+function over3(){
+    whiteText3.style.color='#fff';
+}
+function out3(){
+    blackText3.style.color='#000';
+}
+
+function over4(){
+    whiteText4.style.color='#fff';
+}
+function out4(){
+    blackText4.style.color='#000';
+}
+
+function over5(){
+    whiteText5.style.color='#fff';
+}
+function out5(){
+    blackText5.style.color='#000';
+}
+
+function over6(){
+    whiteText6.style.color='#fff';
+}
+function out6(){
+    blackText6.style.color='#000';
+}
+
+function over7(){
+    whiteText7.style.color='#fff';
+}
+function out7(){
+    blackText7.style.color='#000';
+}
+
+
+
+
+/*--------------------------------------------*/
+
+let menu=document.getElementById('menu-btn');
+let navbar=document.getElementById('header-nav1');
+
+menu.onclick=()=>{
+    navbar.classList.toggle('active');
+}
+
+/*---------------------------------------------*/
+
+
+
